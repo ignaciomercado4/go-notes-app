78e2e0ae54d39b87045135e701cc2e691b2386b0
Ignacio Mercado <igmercado46@gmail.com>
2024-12-07 15:24:28 -0300

    adds db connection to a individual function

--- main.go
+++ main.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"go-notes-app/models"
-
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 func main() {
 	r := gin.Default()
 
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	db.AutoMigrate(&models.User{}, &models.Note{})
+	db := connectDatabase()
 
 	r.LoadHTMLGlob("templates/*")
 
